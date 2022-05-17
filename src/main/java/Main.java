da9d63e14748504c71e7888a372de637a12f15e3
BloodyRanger <BloodyRanger@inbox.ru>
2022-05-18 00:35:25 +0300

    Task1

---
+++ src/main/java/Main.java
@@ -0,0 +1,47 @@
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Main {
+
+    public static final String REMOTE_SERVICE_URI = "https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats";
+    public static ObjectMapper mapper = new ObjectMapper();
+
+    public static void main(String[] args) {
+
+        try {
+            CloseableHttpClient httpClient = HttpClientBuilder.create()
+                    .setDefaultRequestConfig(RequestConfig.custom()
+                            .setConnectTimeout(5000)
+                            .setSocketTimeout(30000)
+                            .setRedirectsEnabled(false)
+                            .build())
+                    .build();
+
+            HttpGet request = new HttpGet(REMOTE_SERVICE_URI);
+            CloseableHttpResponse response = httpClient.execute(request);
+
+            List<Cat> cats = mapper.readValue(response.getEntity().getContent(), new TypeReference<List<Cat>>() {
+            });
+
+            Stream<Cat> stream = cats.stream();
+            stream.filter(value -> value.getUpvotes() > 0)
+                    .forEach(System.out::println);
+
+
+        } catch (IOException err) {
+            err.printStackTrace();
+        }
+
+    }
+}
